c796a4f9b425cc68f49ee14acadaeaaa78cb7e62
Lucas Serafim <lucashenrique74148@gmail.com>
2025-04-01 17:54:58 -0300

    adicionando mascara

---
+++ dist/js/utils/mask.js
@@ -0,0 +1,8 @@
+export function mascara(valor) {
+    let valorAlterado = valor.value;
+    valorAlterado = valorAlterado.replace(/\D/g, ""); // Remove todos os não dígitos
+    valorAlterado = valorAlterado.replace(/(\d+)(\d{2})$/, "$1,$2"); // Adiciona a parte de centavos
+    valorAlterado = valorAlterado.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1."); // Adiciona pontos a cada três dígitos
+    valorAlterado = "R$ " + valorAlterado;
+    valor.value = valorAlterado;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 // src/main.ts
 import { configurarFormulario } from "./components/forms.js";
 import { TransacaoService } from "./services/TransacaoService.js";
-
+import { mascara } from "./utils/mask.js";
 
 
 document.addEventListener("DOMContentLoaded", () => {
