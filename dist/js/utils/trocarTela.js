3511ec82063572d3eb239819220120b01b39a474
Lucas Serafim <lucashenrique74148@gmail.com>
2025-04-04 13:10:41 -0300

    lucas

--- dist/js/utils/trocarTela.js
+++
@@ -1,8 +0,0 @@
-export function alternarDisplay(elemento) {
-    elemento.classList.toggle('d-none');
-    elemento.classList.toggle('d-block');
-}
-export function alternarTelas(telaAtual, telaNova) {
-    alternarDisplay(telaAtual);
-    alternarDisplay(telaNova);
-}
--- dist/main.js
+++
@@ -1,9 +0,0 @@
-// src/main.ts
-import { configurarFormulario } from "./js/components/forms.js";
-import { TransacaoService } from "./js/services/TransacaoService.js";
-import "./js/utils/mask.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-    configurarFormulario();
-    TransacaoService.atualizarExtrato();
-});
